ba19c4321c247af3b72580b2dea29b4ccb24e2ad
Arie Fisher <jbgirl0@gmail.com>
2025-06-10 09:37:50 -0400

    modified my code

--- NorthwindTraders/src/main/java/com/pluralsight/Main.java
+++ NorthwindTraders/src/main/java/com/pluralsight/Main.java
@@ -1,40 +1,68 @@
 package com.pluralsight;
 
-import com.mysql.cj.x.protobuf.MysqlxDatatypes;
-
 import java.sql.*;
-// import javax.sql.DataSource;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            // load the MySQL Driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            // 1. open a connection to the database
-// use the database URL to point to the correct database
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-            String url = "jdbc:mysql://localhost.3306/northwind";
-            String username = "root";
-            String password = "yearup";
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind",
-                    username, password);
-            // create statement
-// the statement is tied to the open connection
-            Statement statement = connection.createStatement();
-            // define your query
-            String query = "SELECT ProductName FROM products";
-            // 2. Execute your query
-            ResultSet results = statement.executeQuery(query);
-// process the results
-            while (results.next()) {
-                String name = results.getString("ProductName");
-                System.out.println(name);
-            }
-// 3. Close the connection
-            connection.close();
+        Class.forName("com.mysql.cj.jdbc.Driver");
 
-        } catch (SQLException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
+        // 2. Set up connection variables
+        String url = "jdbc:mysql://localhost:3306/northwind";
+        String username = "username";  // replace with your MySQL username
+        String password = "password";  // replace with your MySQL password
+
+        // 3. Create connection
+        Connection connection = DriverManager.getConnection(url, username, password);
+
+        // 4. PreparedStatement
+        String query = "SELECT ProductName FROM products";
+        PreparedStatement statement = connection.prepareStatement(query);
+
+        // 5. Execute the query and get results
+        ResultSet results = statement.executeQuery();
+
+        // 6. Loop through results and print each product name
+        while (results.next()) {
+            String productName = results.getString("ProductName");
+            System.out.println(productName);
         }
+
+        // 7. Close all resources
+        results.close();
+        statement.close();
+        connection.close();
     }
 }
+
+//        try {
+//            // load the MySQL Driver
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//            // 1. open a connection to the database
+//// use the database URL to point to the correct database
+//
+//            String url = "jdbc:mysql://localhost.3306/northwind";
+//            String username = "root";
+//            String password = "yearup";
+//            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind",
+//                    username, password);
+//            // create statement
+//// the statement is tied to the open connection
+//            Statement statement = connection.createStatement();
+//            // define your query
+//            String query = "SELECT ProductName FROM products";
+//            // 2. Execute your query
+//            ResultSet results = statement.executeQuery(query);
+//// process the results
+//            while (results.next()) {
+//                String name = results.getString("ProductName");
+//                System.out.println(name);
+//            }
+//// 3. Close the connection
+//            connection.close();
+//
+//        } catch (SQLException | ClassNotFoundException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//}
